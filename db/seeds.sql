566a72a893281bb3f7b9e014bec58c4ac25bfbca
Illgner Alain <illgneralain@gmail.com>
2021-02-15 19:17:54 -0500

    fixed code for seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Molly');
-INSERT INTO cats (name) VALUES ('Charlie');
-INSERT INTO cats (name, sleepy) VALUES ('Poppy', true);
-INSERT INTO cats (name, sleepy) VALUES ('Oscar', true);
-INSERT INTO cats (name, sleepy) VALUES ('Smudge', true);
-INSERT INTO cats (name) VALUES ('Daisy');
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Veggie', false, current_timestamp);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Cheese', false, current_timestamp);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Chicken', false, current_timestamp);
+
