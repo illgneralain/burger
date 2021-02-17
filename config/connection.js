b2ba63e7a2272dd044c8ff0532c680aaf804b699
Illgner Alain <illgneralain@gmail.com>
2021-02-16 23:48:41 -0500

    made corrections to connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,20 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
+const connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-const connection = mysql.createConnection({
+} else {
+  connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
   // NOTE: Be sure to add your MySQL password here!
-  password: '',
+  password: '!llgnerAlain15',
   database: 'burgers_db',
 });
 
+}
 // Make connection.
 connection.connect((err) => {
   if (err) {
